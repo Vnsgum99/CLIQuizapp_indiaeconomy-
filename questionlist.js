80545963aeaa9e1bb650c306789df47c9d476e24
Vnsgum99 <91385949+Vnsgum99@users.noreply.github.com>
2021-10-15 12:41:01 +0530

    Create questionlist.js

---
+++ questionlist.js
@@ -0,0 +1,39 @@
+const questions = [
+        {
+
+
+        question: "In which year Bombay Stock Exchange was established?",
+        answer: "1875",
+        options:  ["1865","1875","1880","1890"]
+        },
+
+
+    {
+        question: "The first step towards electronic bond trading in India was the introduction of the RBIs Negotiated Dealing System. In which year NDS was launched?",
+        answer: "2002",
+        options:  ["2000","2002","2020","2004"]
+        },
+
+    
+    {
+        question: "World Bank Headquarters located at:",
+        answer: "Washington DC",
+        options:  ["Switzerland","New York","Washington DC","Paris"]
+        },
+    
+    {
+        question: "Which among the following correctly categorizes Collateralized Borrowing and Lending Obligation?",
+        answer: "A money market instrument",
+        options:  ["A money market instrument","A market operation","A clearing system","A Scheme of Reserve Bank of India"]
+        }, 
+    
+     {
+        question: "Which among the following is not an indirect tax?",
+        answer: "Corporation Tax",
+        options:  ["Service Tax","Custom Duty","Excise duty","Corporation Tax"]
+        }, 
+      
+      
+      
+]
+module.exports = questions;
